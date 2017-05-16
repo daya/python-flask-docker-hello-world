754950d0e3433e2fa7629e582ec40cd0f66010e0
Daya Sharma <daya.sharma+github@gmail.com>
2017-05-16 15:14:37 -0500

    If running in cluster will print Container ID

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
 from flask import Flask, abort, request
+from subprocess import call, PIPE, Popen
 
 app = Flask(__name__)
 
+def host_name():
+  return Popen("hostname", stdout=PIPE).stdout.read()
+
 @app.route("/")
 def hello():
-    app_name = os.getenv('APP_NAME', "Flask")
-    return "Hello from %s.<br> Data: %s.<br> Description: %s" % (app_name, request.query_string, os.getenv('APP_DESC', "Flask is a microframework for Python based on Werkzeug, Jinja 2 and good intentions"))
+  app_name = os.getenv('APP_NAME', "Flask")
+  app_desc = os.getenv('APP_DESC', "Flask is a microframework for Python based on Werkzeug, Jinja 2 and good intentions")
+
+  return "Hello from %s.<br/> Data: %s.<br/> Description: %s.<br/> ContainerID: %s" % (
+    app_name,
+    request.query_string,
+    app_desc,
+    host_name()
+    )
 
 @app.route("/msg", methods=["POST", "GET"])
 def msg():
@@ -15,3 +26,4 @@ def msg():
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.getenv("FLASK_PORT", 5000)))
+
