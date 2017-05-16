2867f71e285b3ef5d4f0d22b6db43d5a728fd8fd
Daya Sharma <daya.sharma+github@gmail.com>
2017-05-16 13:16:19 -0500

    Upgrading flask and minor changes

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, abort, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     app_name = os.getenv('APP_NAME', "Flask")
-    return "Hello from %s" % app_name
+    return "Hello from %s. Data: %s" % (app_name, request.query_string)
 
+@app.route("/msg", methods=["POST", "GET"])
+def msg():
+  print("Args %s, %s", (request.args.get('name'), request.args.items))
+  return "MSG Received: %s" % (request.data if request.data else request.query_string)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.getenv("FLASK_PORT", 5000)))
